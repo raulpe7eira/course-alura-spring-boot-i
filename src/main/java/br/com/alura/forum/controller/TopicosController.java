966243bc1173427667200a633bb01ec5199b5127
Raul Pereira <raul.pereira@gmail.com>
2020-08-02 03:32:37 -0300

    class 06 - delete topico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -36,6 +36,7 @@ public class TopicosController {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<TopicoDto> cadastrar(
             @RequestBody @Valid TopicoForm form,
             UriComponentsBuilder uriBuilder
@@ -61,6 +62,13 @@ public class TopicosController {
     ) {
         Topico topico = form.atualizar(id, topicoRepository);
         return ResponseEntity.ok(new TopicoDto(topico));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity remover(@PathVariable Long id) {
+        topicoRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 }
