a3dcd96d25825e337a316687608778403ae88404
Raul Pereira <raul.pereira@gmail.com>
2020-08-02 03:28:17 -0300

    class 06 - update topico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,6 +2,7 @@ package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.DetalheTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualizaTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repository.CursoRepository;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
@@ -35,10 +37,10 @@ public class TopicosController {
 
     @PostMapping
     public ResponseEntity<TopicoDto> cadastrar(
-            @RequestBody @Valid TopicoForm topicoForm,
+            @RequestBody @Valid TopicoForm form,
             UriComponentsBuilder uriBuilder
     ) {
-        Topico topico = topicoForm.converter(cursoRepository);
+        Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
 
         URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
@@ -49,6 +51,16 @@ public class TopicosController {
     public DetalheTopicoDto detalhar(@PathVariable Long id) {
         Topico topico = topicoRepository.getOne(id);
         return new DetalheTopicoDto(topico);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<TopicoDto> atualizar(
+            @PathVariable Long id,
+            @RequestBody @Valid AtualizaTopicoForm form
+    ) {
+        Topico topico = form.atualizar(id, topicoRepository);
+        return ResponseEntity.ok(new TopicoDto(topico));
     }
 
 }
